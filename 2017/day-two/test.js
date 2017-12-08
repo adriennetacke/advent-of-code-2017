241238bd9c2b612fd6a90fe59cef22fcb11130c4
Adrienne Tacke <adrienne.tacke@gmail.com>
2017-12-07 19:05:20 -0800

    feature: Add day two, part two test

--- 2017/day-two/test.js
+++ 2017/day-two/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const checksum = require('./checksum-one');
+const checksumDos = require('./checksum-two');
 
 describe('Day Two - Part One', () => {
   it('should properly calculate checksum', () => {
@@ -9,5 +10,16 @@ describe('Day Two - Part One', () => {
      2 4 6 8`
 
     assert.equal(checksum(data), 18);
+  });
+});
+
+describe('Day Two - Part Two', () => {
+  it('should properly calculate second checksum', () => {
+    const data =
+    `5 9 2 8
+     9 4 7 3
+     3 8 6 5`;
+
+     assert.equal(checksumDos(data), 9);
   });
 });
