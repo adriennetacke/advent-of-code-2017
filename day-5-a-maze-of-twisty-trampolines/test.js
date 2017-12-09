cda0cd03e026b98614a9aac43c7c27cd6620110e
Adrienne Tacke <adrienne.tacke@gmail.com>
2017-12-08 22:15:30 -0800

    feature: Add day five, part one test

---
+++ day-5-a-maze-of-twisty-trampolines/test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const twistyTrampoline = require('/twisty-trampoline');
+
+describe ('Day Five - Get Out of Maze One', () => {
+  it('should calculate required steps', () => {
+    const offsets =
+    `0
+     3
+     0
+     1
+     -3`;
+
+    assert.equal(twistyTrampoline(offsets), 5);
+  })
+});
