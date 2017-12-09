86d671ad60ce7c8ebd68cc7d246a386316ea436b
Adrienne Tacke <adrienne.tacke@gmail.com>
2017-12-08 20:40:54 -0800

    feature: Add day four, part two test

--- day-4-high-entropy-passphrases/test.js
+++ day-4-high-entropy-passphrases/test.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
 const passphrase = require('./passphrase');
+const passphraseTwo = require('./passphrase-two.js');
 
-describe ('Day Four - Passphrases', () => {
-  it('should validate aa bb cc dd ee', () => {
+describe ('Day Four - Passphrases Without Duplicates', () => {
+  it('should validate passphrase list one', () => {
     const passphraseList =
     `aa bb cc dd ee
      aa bb cc dd aa
@@ -10,4 +11,17 @@ describe ('Day Four - Passphrases', () => {
 
     assert.equal(passphrase(passphraseList), 2);
   })
-})
+});
+
+describe ('Day Four - Passphrases Without Anagrams', () => {
+  it('should validate passphrase list two', () => {
+    const passphraseList =
+    `abcde fghij
+     abcde xyz ecdab
+     a ab abc abd abf abj
+     iiii oiii ooii oooi oooo
+     oiii ioii iioi iiio`;
+
+    assert.equal(passphraseTwo(passphraseList), 3);
+  });
+});
