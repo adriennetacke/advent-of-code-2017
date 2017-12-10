9642f392d92932625021cc1133350c1faab0ff39
Adrienne Tacke <adrienne.tacke@gmail.com>
2017-12-10 10:41:27 -0800

    feature: Add day 5, part two test

--- day-5-a-maze-of-twisty-trampolines/test.js
+++ day-5-a-maze-of-twisty-trampolines/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const twistyTrampoline = require('./twisty-trampoline');
+const twistyTrampolineTwo = require('./twisty-trampoline-two');
 
 describe ('Day Five - Get Out of Maze One', () => {
   it('should calculate required steps', () => {
@@ -11,5 +12,18 @@ describe ('Day Five - Get Out of Maze One', () => {
      -3`;
 
     assert.equal(twistyTrampoline(offsets), 5);
-  })
+  });
+});
+
+describe ('Day Five - Get Out of Maze Two', () => {
+  it('should calculate required steps', () => {
+    const offsets =
+    `0
+     3
+     0
+     1
+     -3`;
+
+    assert.equal(twistyTrampolineTwo(offsets), 10);
+  });
 });
