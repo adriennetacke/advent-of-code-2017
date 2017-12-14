94ff577996d36ce7913e1ac9126d3b87515db588
Adrienne Tacke <adrienne.tacke@gmail.com>
2017-12-13 18:56:16 -0800

    feature: Add day six, part one test

---
+++ day-6-memory-reallocation/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const memory = require('./memory-reallocation.js');
+
+describe('Day Six - Memory Reallocation', () => {
+  it("should calculate proper reallocation", () => {
+      const memoryBanks =
+      `4	10	4	1	8	4	9	14	5	1	14	15	0	15	3	5`;
+
+      assert.equal(memory(memoryBanks), 5);
+  });
+});
