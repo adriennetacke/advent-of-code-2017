889f76671e670985d83807835e56a5eb10cf1240
Adrienne Tacke <adrienne.tacke@gmail.com>
2017-12-17 14:20:01 -0800

    fix: Add correct test input for part one test

--- day-6-memory-reallocation/test.js
+++ day-6-memory-reallocation/test.js
@@ -4,7 +4,7 @@ const memory = require('./memory-reallocation');
 describe('Day Six - Memory Reallocation', () => {
   it('should calculate proper reallocation', () => {
       const memoryBanks =
-      `4	10	4	1	8	4	9	14	5	1	14	15	0	15	3	5`;
+      `0 2 7 0`;
 
       assert.equal(memory(memoryBanks), 5);
   });
