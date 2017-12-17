4998f8037a0d552e1c53671f13a1460a5e69cea8
Adrienne Tacke <adrienne.tacke@gmail.com>
2017-12-17 14:32:05 -0800

    feature: Add day 6, part two test

--- day-6-memory-reallocation/test.js
+++ day-6-memory-reallocation/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const memory = require('./memory-reallocation');
+const memoryTwo = require('./memory-reallocation-two');
 
 describe('Day Six - Memory Reallocation', () => {
   it('should calculate proper reallocation', () => {
@@ -7,5 +8,14 @@ describe('Day Six - Memory Reallocation', () => {
       `0 2 7 0`;
 
       assert.equal(memory(memoryBanks), 5);
+  });
+});
+
+describe('Day Six - Memory Reallocation ', () => {
+  it('should calculate proper reallocation', () => {
+      const memoryBanks =
+      `0 2 7 0`;
+
+      assert.equal(memoryTwo(memoryBanks), 4);
   });
 });
